832915da0549c66b648a92e076fe06735b728978
Arpan NLN <arpannln@ucla.edu>
2018-05-30 16:06:23 -0700

    got it

--- nutmeg/src/App.jsx
+++ nutmeg/src/App.jsx
@@ -3,35 +3,45 @@ import Home from './components/Home';
 import './App.css';
 
 const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YjBkZWVhMDQ2MjNjNDU3ZjBjMDA4ZDgiLCJpYXQiOjE1Mjc2Mzk3OTJ9.h6cCwvD9WU59nzjWiD1zpOHBBOggyLr1QcfzShtf3qQ';
-let data = { new_access_token: ''};
-
-async function getNewKey () {
-  console.log('hi');
-  data = await fetch(`https://api.sportdeer.com/v1/accessToken?refresh_token=${KEY}`);
-};
 
 class App extends Component {
-
   constructor() {
     super();
-    getNewKey();
 
-    //since this is a temporary key lasting only 30 minutes, let's refresh this key every 29 minutes
-    setInterval(getNewKey, 1740000);
-    this.state = { key: ''};
+    this.state = { key: undefined };
+
   }
 
   componentDidMount() {
-    
+    let that = this;
+
+    //since this is a temporary key lasting only 30 minutes, we have to update this regularly
+    async function getNewKey() {
+      let response = await fetch(`https://api.sportdeer.com/v1/accessToken?refresh_token=${KEY}`);
+      let data = await response.json();
+
+      that.setState({ key: data.new_access_token });
+    }
+
+    getNewKey();
+
+    //incase the user hangs out at our page for more than 30 min
+    setInterval(getNewKey, 1740000);
+
   }
 
+
+
   render() {
-    console.log(this.state.key);
-    return (
+    let key = this.state.key;
+
+    return key ?
+      <div></div> :
+     (
       <div className="App">
-        <Home></Home>
+        <Home key={key}></Home>
       </div>
-    );
+     );
   }
 }
 
