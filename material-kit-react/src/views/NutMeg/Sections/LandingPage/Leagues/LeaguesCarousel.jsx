a825ff73e0c75f67493fcd6ef7275ad5cf755377
Arpan NLN <arpannln@ucla.edu>
2018-06-01 00:52:56 -0700

    just need to create some links for tomorrow also burned so much time on finding pics

--- material-kit-react/src/views/NutMeg/Sections/LandingPage/Leagues/LeaguesCarousel.jsx
+++ material-kit-react/src/views/NutMeg/Sections/LandingPage/Leagues/LeaguesCarousel.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Carousel from "react-slick";
 import imageEPL from "../../../assets/img/EPL.jpg";
+import logoEPL from "../../../assets/img/EPLLogo.jpg";
 import imageLaLiga from "../../../assets/img/LaLiga.jpg";
+import logoLaLiga from "../../../assets/img/LaLigaLogo.png";
 import imageBundesliga from "../../../assets/img/Bundesliga.jpg";
+import logoBundesliga from "../../../assets/img/BundesligaLogo.png";
 import imageWorldCup from "../../../assets/img/WorldCup.jpg";
+import logoWorldCup from "../../../assets/img/WorldCupLogo.png";
 import imageHome from "../../../assets/img/Home.jpg";
-import Badge from "components/Badge/Badge.jsx";
+import Info from "components/Typography/Info.jsx";
 import LocationOn from "@material-ui/icons/LocationOn";
+import Button from "components/CustomButtons/Button.jsx";
 
 class LeaguesCarousel extends React.Component {
   constructor() {
@@ -28,65 +33,83 @@ class LeaguesCarousel extends React.Component {
           <div>
             <h1 className="slick-title slick-white-txt"> NutMeg
             </h1>
-            <h2 className="slick-title-caption"> Breaking the Meta for Soccer Stats </h2>
+            <h3 className="slick-title-caption slick-txt-primary"> Breaking the Meta for Soccer Stats </h3>
             <img
               src={imageHome}
               alt="First slide"
               className="slick-hero"
             />
             <div className="slick-caption">
-              <Badge color="primary">Pick your League</Badge>
+              <Info>
+                Pick your Destination
+              </Info>
             </div>
           </div>
           <div>
+            <img
+              src={logoWorldCup}
+              className="slick-logo absolute large">
+            </img>
+            <Button color="success" className="absolute slick-button">Default</Button>
             <img
               src={imageWorldCup}
               alt="Second slide"
               className="slick-hero"
             />
-            <div className="slick-caption">
-              <h4>
-                <LocationOn className="slick-icons" />Somewhere Beyond,
-                United States
+          <div className="slick-caption">
+              <h4 className="slick-txt-shadow">
+                32 teams 1 cup
               </h4>
             </div>
           </div>
           <div>
+            <img
+              src={logoEPL}
+              className="slick-logo absolute medium">
+            </img>
+            <Button color="success" className="absolute slick-button">Default</Button>
             <img
               src={imageEPL}
               alt="Third slide"
               className="slick-hero"
             />
             <div className="slick-caption">
-              <h4>
-                <LocationOn className="slick-icons" />Yellowstone
-                National Park, United States
+              <h4 className="slick-txt-shadow">
+                Mohammed Salah (Liverpool)
               </h4>
             </div>
           </div>
           <div>
+            <img
+              src={logoLaLiga}
+              className="slick-logo absolute medium">
+            </img>
+            <Button color="success" className="absolute slick-button">Default</Button>
             <img
               src={imageLaLiga}
               alt="Fourth slide"
               className="slick-hero"
             />
             <div className="slick-caption">
-              <h4>
-                <LocationOn className="slick-icons" />Yellowstone
-                National Park, United States
+              <h4 className="slick-txt-shadow">
+                Gareth Bale, Christiano Ronaldo (Real Madrid)
               </h4>
             </div>
           </div>
           <div>
+            <img
+              src={logoBundesliga}
+              className="slick-logo absolute medium">
+            </img>
+            <Button color="success" className="absolute slick-button">Default</Button>
             <img
               src={imageBundesliga}
               alt="Fifth slide"
               className="slick-hero"
             />
             <div className="slick-caption">
-              <h4>
-                <LocationOn className="slick-icons" />Yellowstone
-                National Park, United States
+              <h4 className="slick-txt-shadow">
+                Arjen Robben (Bayern Munich)
               </h4>
             </div>
           </div>
